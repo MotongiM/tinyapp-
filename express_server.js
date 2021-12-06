814847b5c7e4012413b60f3b0e4c64c8b1113773
Michel <Michel_motongi@hotmail.com>
2021-12-06 18:50:10 -0500

    Hello

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,12 +11,21 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls/:shortURL", (req, res) => {
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase};
+    res.render("urls_show", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+    const templateVars = { greeting: 'Hello World!' };
+    res.render("hello_world", templateVars);
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
